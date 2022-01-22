8e99fd00828dc13a48e5e5b4a43944270868f183
Berny Linville <bernylinville@devopsthink.org>
2022-01-22 21:56:19 +0800

    Capturing Input from STDIN

---
+++ todo.v4/cmd/todo/main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/bernylinville/interacting/todo"
+)
+
+// Default file name
+var todoFileName = ".todo.json"
+
+func main() {
+	// Parsing command line flags
+	add := flag.Bool("add", false, "Add task to Todo list")
+	list := flag.Bool("list", false, "List all tasks")
+	complete := flag.Int("complete", 0, "Item to be completed")
+
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(),
+			"%s tool. Developed for The Pragmatic Bookshelf\n", os.Args[0])
+		fmt.Fprintf(flag.CommandLine.Output(), "Copyright 2020\n")
+		fmt.Fprintln(flag.CommandLine.Output(), "Usage information:")
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+
+	// Define an items list
+	l := &todo.List{}
+
+	// Check if the user define the ENV VAR for a custom file name
+	if os.Getenv("TODO_FILENAME") != "" {
+		todoFileName = os.Getenv("TODO_FILENAME")
+	}
+
+	// Use the Get method to read todo items from file
+	if err := l.Get(todoFileName); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	// Decide what to do based on the number of arguments provided
+	switch {
+	// For no extra arguments, print the list
+	case *list:
+		// List current todo items
+		fmt.Print(l)
+	case *complete > 0:
+		// Complete the given item
+		if err := l.Complete(*complete); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		// Save the new list
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	case *add:
+		// When any arguments (excluding flags) are provided, they will be
+		// used as the new task
+		t, err := getTask(os.Stdin, flag.Args()...)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		l.Add(t)
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	default:
+		// Invalid flag provided
+		fmt.Fprintln(os.Stderr, "Invalid option")
+		os.Exit(1)
+	}
+}
+
+// getTask function decides where to get the description for a new task from: arguments or STDIN
+func getTask(r io.Reader, args ...string) (string, error) {
+	if len(args) > 0 {
+		return strings.Join(args, " "), nil
+	}
+
+	s := bufio.NewScanner(r)
+	s.Scan()
+	if err := s.Err(); err != nil {
+		return "", err
+	}
+
+	if len(s.Text()) == 0 {
+		return "", fmt.Errorf("Task cannot be blank")
+	}
+
+	return s.Text(), nil
+}
---
+++ todo.v4/go.mod
@@ -0,0 +1,3 @@
+module github.com/bernylinville/interacting/todo
+
+go 1.17
